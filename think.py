cb7f9702055bbb3105b2b5a8898f0db155a10507
Navjot Singh <navjot8886@gmail.com>
2025-09-12 17:21:08 +0530

    Update think.py

--- think.py
+++ think.py
@@ -23,13 +23,13 @@ print("Aditya" in name)
 # "in" to check if the word exist or not 
 print(len(name))
 #"len" to check the length of the string
-data = ("nick",20,6.1,(4,6,8))
-print(data)
-print(data[3][1])
-print(id(data))
+data3 = ("nick",20,6.1,(4,6,8))
+print(data3)
+print(data3[3][1])
+print(id(data3))
 # mutable id remains same but content or value can be changed and in inmutable id changes 
-print(type(data))
-# "type" to check the type of data
+print(type(data3))
+# "type" to check the type of data3
 data2 = 1,2,3,4,5 
 a,b,c,d,e = data2
 print(a,b,c,d,e)
@@ -52,3 +52,67 @@ print(list1)
 list1.remove(5)
 print(list1)
 #"remove" to remove a value from list 
+
+data3 = {"name" : "john" , "age" :"30" , 1 : "one" , "marks" : [50 , 70, 60], 6.7: "float" , True: 1}
+print(data3)
+print(data3["age"])
+print(data3.get("age"))
+#upper 2 are the ways to get the value
+for key in data3:
+    print(key)
+# in this the "in" is used to find the "key" in "data3"
+for i in data3:
+    print(i, data3[i])
+data3["name"] = "nick"
+print(data3)
+#to change the current value
+data3["gender"] = "male"
+print(data3)
+#it can also be used to add a new value
+data3.pop("age")
+data3.popitem()
+del data3["marks"]
+data3.clear()
+print(data3)
+#"pop" was used to remove a specific key and "popitem" was used to remove the last key and "del" is used to "delete" the key and its reference in memory too and to clear all we use "clear"
+print(len(data3))
+#to find length
+
+data4 = dict(name = "fury" , age= 43)
+print(data4)
+print(type(data4))
+
+data5 = dict(("name" , "cap") , ("age" , 96))
+print(data5)
+
+data6 = set()
+print(type(data6))
+data6 = {"doc",54,64,86}
+print(id(data6))
+data6.add("jog")
+print(id(data6))
+#both id is same which means set is muatable
+data7=data6
+print(id(data7))
+#same id ayegi
+data8 = data6.copy()
+print(data8)
+#".copy" to copy a set and get a different id 
+data6.remove("jog")
+data6.discard("dac")
+print(data6)
+#both ".remove and .discard" is used for the same perpose but discard will not give any error for example "dac"is a wrong spelling but still no error
+
+data9 ={"name" :"brock" , "age" : 23}
+if "age" in data9:
+    print("votingg")
+else:
+    print("not eligible to vote")
+# "age" is present in dict if we find "date" then it will show the else statement
+
+print(range(1 , 10))
+for i in range(1,11):
+    print(i) 
+
+for i in range(3):
+    print("hello")
